b9fe314dd5d3ffde5f6509930776d07d382e2c07
Amritha Pallavoor <amrithasp02@gmail.com>
2026-05-13 23:28:11 +0000

    move postprocess.py into a subfolder and add __init__.py

---
+++ docker-photogrammetry-postprocessing/postprocessing/__init__.py
@@ -0,0 +1,9 @@
+from .postprocess import (
+    create_dir,
+    lonlat_to_utm_epsg,
+    transform_to_local_utm,
+    crop_raster_save_cog,
+    make_chm,
+    create_thumbnail,
+    postprocess_photogrammetry_containerized,
+)
