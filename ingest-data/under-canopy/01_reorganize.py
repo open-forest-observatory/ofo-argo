6cf3ff31c80a27cc3866607dc975b03a1ecbe262
David Russell <djrussell@ucdavis.edu>
2026-07-22 16:41:46 +0000

    Update the gopro prefixes so it does not interfere with the csv read

--- ingest-data/under-canopy/01_reorganize.py
+++ ingest-data/under-canopy/01_reorganize.py
@@ -29,8 +29,9 @@ def parse_args():
     parser.add_argument(
         "--gopro-file-prefix",
         type=str,
+        nargs="+",
         required=True,
-        help="One or more GoPro file prefixes for this mission, comma-separated.",
+        help="One or more GoPro file prefixes for this mission.",
     )
 
     return parser.parse_args()
@@ -38,7 +39,7 @@ def parse_args():
 
 def main(input_data_folder, output_data_folder, ofo_mission_id, gopro_file_prefix):
     # Split the gopro_collect_id by commas and remove whitespace to get all included prefixes
-    gopro_file_prefixes = [x.strip() for x in gopro_file_prefix.split(",")]
+    gopro_file_prefixes = [x.strip() for x in gopro_file_prefix]
 
     # Find all files matching any prefixes
     matching_files = [
