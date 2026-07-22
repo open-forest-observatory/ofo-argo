1cff8f0c93763cedef2b64b5b4eb1a5fecb3779e
David Russell <djrussell@ucdavis.edu>
2026-07-22 16:16:33 +0000

    Update reorganize step to only process one dataset

--- ingest-data/under-canopy/01_reorganize.py
+++ ingest-data/under-canopy/01_reorganize.py
@@ -5,8 +5,6 @@ import os
 import json
 import shutil
 
-import pandas as pd
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -23,57 +21,58 @@ def parse_args():
         type=Path,
     )
     parser.add_argument(
-        "mission_file",
-        type=Path,
-        help="This should have the following fields: 'ofo_mission_id' and 'gopro_file_prefix'",
+        "--ofo-mission-id",
+        type=int,
+        required=True,
+        help="The OFO mission ID for this dataset.",
+    )
+    parser.add_argument(
+        "--gopro-file-prefix",
+        type=str,
+        required=True,
+        help="One or more GoPro file prefixes for this mission, comma-separated.",
     )
 
     return parser.parse_args()
 
 
-def main(input_data_folder, output_data_folder, mission_file):
-    missions = pd.read_csv(mission_file)
+def main(input_data_folder, output_data_folder, ofo_mission_id, gopro_file_prefix):
+    # Split the gopro_collect_id by commas and remove whitespace to get all included prefixes
+    gopro_file_prefixes = [x.strip() for x in gopro_file_prefix.split(",")]
 
-    for _, row in missions.iterrows():
-        ofo_mission_id = row.ofo_mission_id
-        gopro_file_prefix = row.gopro_file_prefix
+    # Find all files matching any prefixes
+    matching_files = [
+        f
+        for f in input_data_folder.rglob("*")
+        if re.match("|".join(re.escape(p) for p in gopro_file_prefixes), f.name)
+    ]
 
-        # Split the gopro_collect_id by commas and remove whitespace to get all included prefixes
-        gopro_file_prefixes = [x.strip() for x in gopro_file_prefix.split(",")]
+    # Sort files by name.
+    # TODO we might want to parse the timestamp in the future but the names should be consecutive in time.
+    matching_files = sorted(matching_files)
 
-        # Find all files matching any prefixes
-        matching_files = [
-                f
-                for f in input_data_folder.rglob("*")
-                if re.match("|".join(re.escape(p) for p in gopro_file_prefixes), f.name)
-        ]
+    # TODO: Consider a check to see whether this is within the start/end timestamps and/or if there are timestamp gaps
+    # This would require first parsing the metadata
 
-        # Sort files by name. 
-        # TODO we might want to parse the timestamp in the future but the names should be consecutive in time.
-        matching_files = sorted(matching_files)
+    # Create an output folder based on the output folder / ofo_mission_id
+    output_folder = Path(output_data_folder, f"{ofo_mission_id:06d}")
+    # remove old folder, if present
+    shutil.rmtree(output_folder, ignore_errors=True)
+    # And recreate
+    output_folder.mkdir(parents=True, exist_ok=True)
 
-        # TODO: Consider a check to see whether this is within the start/end timestamps and/or if there are timestamp gaps
-        # This would require first parsing the metadata
+    # Hardlink all files to that location
+    # The output format should be <mission_id>/<mission_id>-<image_id>.JPG
+    filename_remapping = {
+        str(in_f): str(Path(output_folder, f"{ofo_mission_id:06d}_{i:06d}.JPG"))
+        for i, in_f in enumerate(matching_files)
+    }
 
-        # Create an output folder based on the output folder / ofo_mission_id
-        output_folder = Path(output_data_folder, f"{ofo_mission_id:06d}")
-        # remove old folder
-        shutil.rmtree(output_folder)
-        # And recreate
-        output_folder.mkdir(parents=True, exist_ok=True)
+    for in_f, out_f in filename_remapping.items():
+        os.link(in_f, out_f)
 
-        # Hardlink all files to that location
-        # The output format should be <mission_id>/<mission_id>-<image_id>.JPG
-        filename_remapping = {
-            str(in_f): str(Path(output_folder, f"{ofo_mission_id:06d}_{i:06d}.JPG"))
-            for i, in_f in enumerate(matching_files)
-        }
-
-        for in_f, out_f in filename_remapping.items():
-            os.link(in_f, out_f)
-
-        with open(Path(output_folder, "filename-remapping.json"), "w") as outfile_h:
-            json.dump(filename_remapping, outfile_h)
+    with open(Path(output_folder, "filename-remapping.json"), "w") as outfile_h:
+        json.dump(filename_remapping, outfile_h)
 
 
 if __name__ == "__main__":
