f56bfa59067ac0e7dea57f30a0b32dc9175d3230
russelldj <29384527+russelldj@users.noreply.github.com>
2026-09-22 19:57:31 +0000

    Apply isort formatting changes

--- docker-photogrammetry-postprocessing/postprocessing/__init__.py
+++ docker-photogrammetry-postprocessing/postprocessing/__init__.py
@@ -1,9 +1,4 @@
-from .postprocess import (
-    create_dir,
-    create_thumbnail,
-    crop_raster_save_cog,
-    lonlat_to_utm_epsg,
-    make_chm,
-    postprocess_photogrammetry_containerized,
-    transform_to_local_utm,
-)
+from .postprocess import (create_dir, create_thumbnail, crop_raster_save_cog,
+                          lonlat_to_utm_epsg, make_chm,
+                          postprocess_photogrammetry_containerized,
+                          transform_to_local_utm)
