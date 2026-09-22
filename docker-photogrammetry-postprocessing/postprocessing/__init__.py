7ac12c48bd9d86a30e5904269b2a8112d5e60faa
russelldj <29384527+russelldj@users.noreply.github.com>
2026-09-22 18:45:47 +0000

    Apply black formatting changes

--- docker-photogrammetry-postprocessing/postprocessing/__init__.py
+++ docker-photogrammetry-postprocessing/postprocessing/__init__.py
@@ -1,4 +1,9 @@
-from .postprocess import (create_dir, create_thumbnail, crop_raster_save_cog,
-                          lonlat_to_utm_epsg, make_chm,
-                          postprocess_photogrammetry_containerized,
-                          transform_to_local_utm)
+from .postprocess import (
+    create_dir,
+    create_thumbnail,
+    crop_raster_save_cog,
+    lonlat_to_utm_epsg,
+    make_chm,
+    postprocess_photogrammetry_containerized,
+    transform_to_local_utm,
+)
